172b35116e21bb17e72cc2236fee005c6e590745
Leonardo Behnck <leonardo.behnck@gmail.com>
2024-04-17 16:33:46 -0300

    ajuste form.

--- scripts/main.js
+++ scripts/main.js
@@ -3,7 +3,3 @@ function erase() {
   document.getElementById("email").value = ""
   document.getElementById("msg").value = ""
 }
-
-function send() {
-  alert("Obrigado por enviar sua mensagem!")
-}
